fc3e81400af2f2e0bee801d0e5fe863d96de0472
Mr Garrix RL <rlfernando7@gmail.com>
2020-09-27 13:20:31 -0500

    Create link pages. Implement styles in header

--- index.php
+++ index.php
@@ -3,9 +3,37 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|PT+Sans:400,700" rel="stylesheet">
 	<title>Blog Cafe</title>
 </head>
 <body>
+
+	<!--============================
+	=            HEADER            =
+	=============================-->
+	<header class="site-header">
+		<div class="contenedor">
+			<div class="barra">
+				<a href="/">
+					<h1 class="no-margin">Blog<span>DeCafé</span></h1>
+				</a>
+				<nav class="navegacion">
+					<a href="nosotros.php">Nosotros</a>
+					<a href="cursos.php">Cursos</a>
+					<a href="contacto.php">Contacto</a>
+				</nav>
+			</div><!--End barra-->
+			<div class="texto-header">
+				<h2 class="no-margin">Blog de Café con consejos y cursos</h2>
+				<p class="no-margin">Lorem ipsum dolor sit amet consectetur, adipisicing elit. In doloribus, molestias sequi corrupti et, aliquam labore unde velit sit, qui, iste accusamus! Sequi deleniti nemo voluptatibus amet fugit architecto praesentium.</p>
+			</div>
+		</div><!--End contenedor-->
+	</header>
+	
+	
+	<!--====  End of HEADER  ====-->
+	
 	
 </body>
 </html>
