6b7b968c16a41d54d623778a17339ce732aaf887
Mr Garrix RL <rlfernando7@gmail.com>
2020-09-27 16:49:28 -0500

    add normalize and implement styles for contenido principal with mobile first

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 	<link rel="stylesheet" href="css/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans|PT+Sans:400,700" rel="stylesheet">
 	<title>Blog Cafe</title>
@@ -30,9 +31,73 @@
 			</div>
 		</div><!--End contenedor-->
 	</header>
-	
-	
 	<!--====  End of HEADER  ====-->
+
+	<!--=========================================
+	=            Contenido-Principal            =
+	==========================================-->
+	<div class="contenido-principal contenido">
+		<main class="blog">
+			<h2>Nuestro Blog</h2>
+			<!--Article-->
+			<article class="entrada-blog">
+				<div class="imagen">
+					<img src="img/blog1.jpg" alt="Imagen blog">
+				</div>
+				<div class="contenido-blog">
+					<h3>Tipos de grano de café</h3>
+					<p>Lorem, ipsum dolor sit, amet consectetur adipisicing elit. Pariatur libero dolores eaque officia quibusdam impedit. Quasi officia optio expedita odio iure nam cumque velit, temporibus sit tempora fugit impedit recusandae?</p>
+					<a href="entrada.php" class="btn btn-primario">Leer entrada</a>
+				</div>
+			</article>
+			<!--Article-->
+			<article class="entrada-blog">
+				<div class="imagen">
+					<img src="img/blog2.jpg" alt="Imagen blog">
+				</div>
+				<div class="contenido-blog">
+					<h3>3 Deliciosas recetas para café</h3>
+					<p>Lorem, ipsum dolor sit, amet consectetur adipisicing elit. Pariatur libero dolores eaque officia quibusdam impedit. Quasi officia optio expedita odio iure nam cumque velit, temporibus sit tempora fugit impedit recusandae?</p>
+					<a href="entrada.php" class="btn btn-primario">Leer entrada</a>
+				</div>
+			</article>
+			<!--Article-->
+			<article class="entrada-blog">
+				<div class="imagen">
+					<img src="img/blog3.jpg" alt="Imagen blog">
+				</div>
+				<div class="contenido-blog">
+					<h3>Beneficios del café</h3>
+					<p>Lorem, ipsum dolor sit, amet consectetur adipisicing elit. Pariatur libero dolores eaque officia quibusdam impedit. Quasi officia optio expedita odio iure nam cumque velit, temporibus sit tempora fugit impedit recusandae?</p>
+					<a href="entrada.php" class="btn btn-primario">Leer entrada</a>
+				</div>
+			</article>
+		</main>
+		<!--Aside cursos-->
+		<aside class="cursos">
+			<h2>Nuestros cursos y talleres</h2>
+			<ul class="cursos-lista">
+				<!--Curso-->
+				<li class="curso">
+					<h4>Tecnicas de extraccion de café</h4>
+					<p>Precio: <span>Gratis</span></p>
+					<p>Cupo: <span>20</span></p>
+					<a href="cursos.php" class="btn btn-secundario">Más información</a>
+				</li>
+				<!--Curso-->
+				<li class="curso">
+					<h4>4 Recetas de cafe para principiantes</h4>
+					<p>Precio: <span>Gratis</span></p>
+					<p>Cupo: <span>20</span></p>
+					<a href="cursos.php" class="btn btn-secundario">Más información</a>
+				</li>
+			</ul>
+		</aside>
+	</div>
+	
+	
+	<!--====  End of Contenido-Principal  ====-->
+	
 	
 	
 </body>
